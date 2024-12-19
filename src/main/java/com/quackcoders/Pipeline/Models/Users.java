8ba58a042f68634f8e6c3db8cdc3074510ce62da
mibo0419 <bolanosm95@gmail.com>
2024-12-18 22:45:51 -0500

    maintenance requests model

--- src/main/java/com/quackcoders/Pipeline/Models/Users.java
+++ src/main/java/com/quackcoders/Pipeline/Models/Users.java
@@ -1,12 +1,15 @@
 package com.quackcoders.Pipeline.Models;
 
 import lombok.Data;
+import lombok.NonNull;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "Users")
 public class Users {
-    @id
+    @Id
     private String id;
     private String name;
     private String email;
