898dc5b4b1bce263db917ff2e07dc1c7d7aa743c
Eric Vazquez <ericvazquez7992@gmail.com>
2025-02-25 22:29:32 -0500

    finished crud options

--- src/main/java/com/quackcoders/Pipeline/Repository/RequestRepository.java
+++ src/main/java/com/quackcoders/Pipeline/Repository/RequestRepository.java
@@ -3,7 +3,15 @@ package com.quackcoders.Pipeline.Repository;
 import com.quackcoders.Pipeline.Model.Request;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RequestRepository extends MongoRepository <Request ,String> {
 
 
+        List<Request> findAll();
+
+    void deleteById(String id);
+
+//      Request findById();
 }
--- src/main/java/com/quackcoders/Pipeline/Service/RequestService.java
+++ src/main/java/com/quackcoders/Pipeline/Service/RequestService.java
@@ -8,7 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class RequestService {
@@ -18,8 +20,59 @@ public class RequestService {
 
     public Request generateRequest(Map<String,String> request) {
         log.info(request.toString());
-        Request newest = new Request(request.get("type"));
+        Request newest = new Request(request.get("type"), request.get("description"));
 
         return requestRepository.insert(newest);
+    }
+
+    public Request updateRequest(Map<String, String> request, String id) {
+        // description, type, status
+
+        Optional<Request> currentRequest = (requestRepository.findById(id));
+        Request updatedRequest = new Request();
+        currentRequest.ifPresent(req ->{
+            updatedRequest.setId(req.getId());
+            updatedRequest.setSubmittedDate(req.getSubmittedDate());
+            updatedRequest.setTenant(req.getTenant());
+            updatedRequest.setAssignee(req.getAssignee());
+            updatedRequest.setClosedDate(req.getClosedDate());
+            updatedRequest.setType(req.getType());
+            updatedRequest.setStatus(req.getStatus());
+            updatedRequest.setDescription(req.getDescription());
+        });
+            if(request.containsKey("status")){
+                updatedRequest.setStatus(request.get("status"));
+            }   if(request.containsKey("type")){
+                updatedRequest.setType(request.get("type"));
+            }   if(request.containsKey("description")){
+                updatedRequest.setDescription(request.get("description"));
+            }
+
+            return requestRepository.save(updatedRequest);
+
+    }
+
+    public void deleteRequestById(String id){
+        Request deletedRequest = new Request();
+
+        if(requestRepository.existsById(id)){
+
+            requestRepository.deleteById(id);
+            System.out.println("Deleted Successfully/Yeeted out of existence");
+        }
+        else{
+            System.out.println("Nothing was deleted");
+        }
+    }
+
+
+    public Optional<Request> getRequestById(String id) {
+        Optional<Request> getSpecificRequest;
+        getSpecificRequest = requestRepository.findById(id);
+        return getSpecificRequest;
+    }
+
+    public List<Request> getAllRequests(){
+        return requestRepository.findAll();
     }
 }
